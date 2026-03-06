f02d4348804f7e8ae534f45122a85f4a4ad935c9
Aleksandr86rus <168619858+Aleksandr86rus@users.noreply.github.com>
2026-03-07 01:18:08 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,21 @@
-const CACHE_NAME = 'shift-calendar-v2026';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = 'arrizo-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
-// Установка: кешируем файлы для работы без интернета
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Активация: чистим старый кеш, если обновили версию
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Обработка запросов: сначала смотрим в кеш, потом в сеть
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
